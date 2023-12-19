9265d751d79dd07caa219f721758e03d05ecf65e
Doxam1 <143098976+doxam1@users.noreply.github.com>
2023-12-19 22:54:29 +0200

    added css styles to page

---
+++ script.js
@@ -0,0 +1,6 @@
+// add variables inside a functions so i can use it all over.
+
+// add clicks listiners for sqaures and connect it to the board array.
+
+// do a dialog for user entries - names, and game choice.
+
